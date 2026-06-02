ba3dfc4839a8bdfa71a3e20635f0d84bb7ddec0b
Uttam Kumar <uttamutkarsh512@gmail.com>
2026-06-02 14:09:54 +0530

    Avoid duplicate brand logo text

--- frontend/src/components/BrandLogo.jsx
+++ frontend/src/components/BrandLogo.jsx
@@ -3,14 +3,11 @@ import { Link } from 'react-router-dom'
 
 function BrandLogo({ to = '/', showText = true, className = '', textClassName = 'text-xl' }) {
   const content = (
-    <>
-      <img src="/logo.svg" alt="" className="h-9 w-9 shrink-0 object-contain" />
-      {showText && (
-        <span className={`${textClassName} font-bold bg-gradient-to-r from-emerald-600 to-cyan-600 bg-clip-text text-transparent`}>
-          PingBEAT
-        </span>
-      )}
-    </>
+    <img
+      src={showText ? '/logo.svg' : '/favicon.svg'}
+      alt="PingBEAT"
+      className={showText ? 'h-10 w-36 shrink-0 object-cover object-center' : 'h-9 w-9 shrink-0 object-contain'}
+    />
   )
 
   if (!to) {
