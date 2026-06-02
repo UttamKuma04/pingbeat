bf57d2153bb789a204f56ea6cb6de68791ab490e
Uttam Kumar <uttamutkarsh512@gmail.com>
2026-06-03 01:56:24 +0530

    Updated version1.0

---
+++ frontend/src/googleAnalytics.js
@@ -0,0 +1,20 @@
+export function initGoogleAnalytics() {
+  const id = import.meta.env.VITE_MEASUREMENT_ID || ''
+  if (!id) return
+
+  if (window && window.gtag) {
+    try { window.gtag('config', id) } catch (e) {}
+    return
+  }
+
+  const script = document.createElement('script')
+  script.async = true
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${id}`
+  document.head.appendChild(script)
+
+  window.dataLayer = window.dataLayer || []
+  function gtag(){window.dataLayer.push(arguments)}
+  window.gtag = gtag
+  gtag('js', new Date())
+  gtag('config', id)
+}
